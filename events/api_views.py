58fc05baade830c695c7244ce52d6942f5dcd5eb
Dennie <oculareo@gmail.com>
2023-10-02 16:22:50 -0400

    Completed list and show views for attendees, events and presentation views

--- events/api_views.py
+++ events/api_views.py
@@ -1,27 +1,9 @@
 from django.http import JsonResponse
-
 from .models import Conference, Location
 
 
+# API_LIST_CONFERENCES
 def api_list_conferences(request):
-    """
-    Lists the conference names and the link to the conference.
-
-    Returns a dictionary with a single key "conferences" which
-    is a list of conference names and URLS. Each entry in the list
-    is a dictionary that contains the name of the conference and
-    the link to the conference's information.
-
-    {
-        "conferences": [
-            {
-                "name": conference's name,
-                "href": URL to the conference,
-            },
-            ...
-        ]
-    }
-    """
     response = []
     conferences = Conference.objects.all()
     for conference in conferences:
@@ -34,32 +16,10 @@ def api_list_conferences(request):
     return JsonResponse({"conferences": response})
 
 
+# API_SHOW_CONFERENCE
 def api_show_conference(request, id):
-    """
-    Returns the details for the Conference model specified
-    by the id parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
-    {
-        "name": the conference's name,
-        "starts": the date/time when the conference starts,
-        "ends": the date/time when the conference ends,
-        "description": the description of the conference,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "max_presentations": the maximum number of presentations,
-        "max_attendees": the maximum number of attendees,
-        "location": {
-            "name": the name of the location,
-            "href": the URL for the location,
-        }
-    }
-    """
     conference = Conference.objects.get(id=id)
+
     return JsonResponse(
         {
             "name": conference.name,
@@ -78,43 +38,32 @@ def api_show_conference(request, id):
     )
 
 
+# API_LIST_LOCATIONS
 def api_list_locations(request):
-    """
-    Lists the location names and the link to the location.
-
-    Returns a dictionary with a single key "locations" which
-    is a list of location names and URLS. Each entry in the list
-    is a dictionary that contains the name of the location and
-    the link to the location's information.
-
-    {
-        "locations": [
+    response = []
+    locations = Location.objects.all()
+    for location in locations:
+        response.append(
             {
-                "name": location's name,
-                "href": URL to the location,
-            },
-            ...
-        ]
-    }
-    """
-    return JsonResponse({})
+                "name": location.name,
+                "href": location.get_api_url(),
+            }
+        )
+    return JsonResponse({"locations": response})
 
 
+# API SHOW LOCATION
 def api_show_location(request, id):
-    """
-    Returns the details for the Location model specified
-    by the id parameter.
-
-    This should return a dictionary with the name, city,
-    room count, created, updated, and state abbreviation.
-
-    {
-        "name": location's name,
-        "city": location's city,
-        "room_count": the number of rooms available,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "state": the two-letter abbreviation for the state,
-    }
-    """
-    return JsonResponse({})
+    location = Location.objects.get(id=id)
+    return JsonResponse(
+        {
+            "name": location.name,
+            "city": location.city,
+            "room_count": location.room_count,
+            "created": location.created,
+            "updated": location.updated,
+            "state": {
+                "name": location.state.name,
+            },
+        }
+    )
--- presentations/api_views.py
+++ presentations/api_views.py
@@ -3,28 +3,8 @@ from django.http import JsonResponse
 from .models import Presentation
 
 
+# API_LIST_PRESENTATION
 def api_list_presentations(request, conference_id):
-    """
-    Lists the presentation titles and the link to the
-    presentation for the specified conference id.
-
-    Returns a dictionary with a single key "presentations"
-    which is a list of presentation titles and URLS. Each
-    entry in the list is a dictionary that contains the
-    title of the presentation, the name of its status, and
-    the link to the presentation's information.
-
-    {
-        "presentations": [
-            {
-                "title": presentation's title,
-                "status": presentation's status name
-                "href": URL to the presentation,
-            },
-            ...
-        ]
-    }
-    """
     presentations = [
         {
             "title": p.title,
@@ -36,29 +16,22 @@ def api_list_presentations(request, conference_id):
     return JsonResponse({"presentations": presentations})
 
 
+# API_SHOW_PRESENTATION
 def api_show_presentation(request, id):
-    """
-    Returns the details for the Presentation model specified
-    by the id parameter.
 
-    This should return a dictionary with the presenter's name,
-    their company name, the presenter's email, the title of
-    the presentation, the synopsis of the presentation, when
-    the presentation record was created, its status name, and
-    a dictionary that has the conference name and its URL
-
-    {
-        "presenter_name": the name of the presenter,
-        "company_name": the name of the presenter's company,
-        "presenter_email": the email address of the presenter,
-        "title": the title of the presentation,
-        "synopsis": the synopsis for the presentation,
-        "created": the date/time when the record was created,
-        "status": the name of the status for the presentation,
-        "conference": {
-            "name": the name of the conference,
-            "href": the URL to the conference,
+    presentation = Presentation.objects.get(id=id)
+    return JsonResponse(
+        {
+            "presenter_name": presentation.presenter_name,
+            "company_name": presentation.company_name,
+            "presenter_email": presentation.presenter_email,
+            "title": presentation.title,
+            "synopsis": presentation.synopsis,
+            "created": presentation.created,
+            "status": {"status": presentation.status.name},
+            "conference": {
+                "name": presentation.conference.name,
+                "href": presentation.conference.get_api_url(),
+            },
         }
-    }
-    """
-    return JsonResponse({})
+    )
